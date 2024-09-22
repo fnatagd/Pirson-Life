0a2f660c4c52aca65418bbb4676e4fb736ddf8da
fnatagd <galikegor7@gmail.com>
2024-09-22 20:45:30 +0300

    lua.lua

--- lua.lua
+++ lua.lua
@@ -1,35 +1,23 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/cat"))()
-
 local Window = Library:CreateWindow("Exemin HUB", Vector3.new(552, 668), Enum.KeyCode.RightControl)
-
 local AimingTab = Window:CreateTab("Pirson Life")
-
-
 local testSection = AimingTab:CreateSector("Humanoid", "left")
-
-
 testSection:AddTextbox("Walk Speed Change", nil, function(State)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = State
 end)
-
 testSection:AddButton("Reset Walk Speed", function(IhateGayPeople)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 end)
-
 testSection:AddTextbox("Jump Power Change", nil, function(State)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = State
 end)
-
 testSection:AddButton("Reset Jump Power", function(IhateGayPeople)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
 end)
-
 local testSection = AimingTab:CreateSector("Humanoid Misc", "left")
-
 testSection:AddButton("Noclip (Rejoin To Off)", function(IhateGayPeople)
     local Noclip = nil
 local Clip = nil
-
 function noclip()
 	Clip = false
 	local function Nocl()
@@ -41,38 +29,29 @@ function noclip()
 			end
 		end
 		wait(0.21) -- basic optimization
-	end
+			end
 	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
 end
-
 function clip()
 	if Noclip then Noclip:Disconnect() end
 	Clip = true
 end
-
 noclip() -- to toggle noclip() and clip()
 end)
-
 testSection:AddToggle("Freeze Player", false, function(first)
     game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = first
 end)
-
 local testSection = AimingTab:CreateSector("Teleport", "left")
-
 testSection:AddButton("Hall", function(IhateGayPeople)
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(918.77,100,2379.07) 
 end)
-
 testSection:AddButton("Yard", function(IhateGayPeople)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(779.87,98,2458.93)
 end)
-
 testSection:AddButton("Criminal Basse", function(IhateGayPeople)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-943.46,94.13,2063.63)
 end)
-
 local testSection = AimingTab:CreateSector("Combat", "right")
-
 testSection:AddButton("Kill Aura", function(IhateGayPeople)
     while wait () do
             for i, e in pairs(game.Players:GetChildren()) do
@@ -83,16 +62,12 @@ testSection:AddButton("Kill Aura", function(IhateGayPeople)
       end 
    end 
 end)
-
-
 testSection:AddButton("Fling All", function(IhateGayPeople)
     local Targets = {"All"} -- "All", "Target Name", "arian_was_here"
 
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
-
 local AllBool = false
-
 local GetPlayer = function(Name)
     Name = Name:lower()
     if Name == "all" or Name == "others" then
@@ -116,23 +91,19 @@ local GetPlayer = function(Name)
         return
     end
 end
-
 local Message = function(_Title, _Text, Time)
     game:GetService("StarterGui"):SetCore("SendNotification", {Title = _Title, Text = _Text, Duration = Time})
 end
-
 local SkidFling = function(TargetPlayer)
     local Character = Player.Character
     local Humanoid = Character and Character:FindFirstChildOfClass("Humanoid")
     local RootPart = Humanoid and Humanoid.RootPart
-
     local TCharacter = TargetPlayer.Character
     local THumanoid
     local TRootPart
     local THead
     local Accessory
     local Handle
-
     if TCharacter:FindFirstChildOfClass("Humanoid") then
         THumanoid = TCharacter:FindFirstChildOfClass("Humanoid")
     end
@@ -148,7 +119,6 @@ local SkidFling = function(TargetPlayer)
     if Accessoy and Accessory:FindFirstChild("Handle") then
         Handle = Accessory.Handle
     end
-
     if Character and Humanoid and RootPart then
         if RootPart.Velocity.Magnitude < 50 then
             getgenv().OldPos = RootPart.CFrame
@@ -166,69 +136,51 @@ local SkidFling = function(TargetPlayer)
         if not TCharacter:FindFirstChildWhichIsA("BasePart") then
             return
         end
-        
         local FPos = function(BasePart, Pos, Ang)
             RootPart.CFrame = CFrame.new(BasePart.Position) * Pos * Ang
             Character:SetPrimaryPartCFrame(CFrame.new(BasePart.Position) * Pos * Ang)
             RootPart.Velocity = Vector3.new(9e7, 9e7 * 10, 9e7)
             RootPart.RotVelocity = Vector3.new(9e8, 9e8, 9e8)
         end
-        
         local SFBasePart = function(BasePart)
             local TimeToWait = 2
             local Time = tick()
             local Angle = 0
-
             repeat
                 if RootPart and THumanoid then
                     if BasePart.Velocity.Magnitude < 50 then
                         Angle = Angle + 100
-
                         FPos(BasePart, CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle),0 ,0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, -1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(2.25, 1.5, -2.25) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(-2.25, -1.5, 2.25) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection,CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, -1.5, 0) + THumanoid.MoveDirection,CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
                     else
                         FPos(BasePart, CFrame.new(0, 1.5, THumanoid.WalkSpeed), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, -1.5, -THumanoid.WalkSpeed), CFrame.Angles(0, 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, 1.5, THumanoid.WalkSpeed), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
-                        
                         FPos(BasePart, CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, -1.5, -TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(0, 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(0, 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, -1.5 ,0), CFrame.Angles(math.rad(-90), 0, 0))
                         task.wait()
-
                         FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(0, 0, 0))
                         task.wait()
                     end
@@ -237,7 +189,6 @@ local SkidFling = function(TargetPlayer)
                 end
             until BasePart.Velocity.Magnitude > 500 or BasePart.Parent ~= TargetPlayer.Character or TargetPlayer.Parent ~= Players or not TargetPlayer.Character == TCharacter or THumanoid.Sit or Humanoid.Health <= 0 or tick() > Time + TimeToWait
         end
-        
         workspace.FallenPartsDestroyHeight = 0/0
         
         local BV = Instance.new("BodyVelocity")
@@ -245,9 +196,7 @@ local SkidFling = function(TargetPlayer)
         BV.Parent = RootPart
         BV.Velocity = Vector3.new(9e8, 9e8, 9e8)
         BV.MaxForce = Vector3.new(1/0, 1/0, 1/0)
-        
         Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, false)
-        
         if TRootPart and THead then
             if (TRootPart.CFrame.p - THead.CFrame.p).Magnitude > 5 then
                 SFBasePart(THead)
@@ -263,11 +212,9 @@ local SkidFling = function(TargetPlayer)
         else
             return Message("Error Occurred", "Target is missing everything", 5)
         end
-        
         BV:Destroy()
         Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, true)
         workspace.CurrentCamera.CameraSubject = Humanoid
-        
         repeat
             RootPart.CFrame = getgenv().OldPos * CFrame.new(0, .5, 0)
             Character:SetPrimaryPartCFrame(getgenv().OldPos * CFrame.new(0, .5, 0))
@@ -284,22 +231,17 @@ local SkidFling = function(TargetPlayer)
         return Message("Error Occurred", "Random error", 5)
     end
 end
-
 if not Welcome then Message("", "", 5) end
 getgenv().Welcome = true
 if Targets[1] then for _,x in next, Targets do GetPlayer(x) end else return end
-
 if AllBool then
     for _,x in next, Players:GetPlayers() do
         SkidFling(x)
     end
 end
-
 for _,x in next, Targets do
     if GetPlayer(x) and GetPlayer(x) ~= Player then
         if GetPlayer(x).UserId ~= 1414978355 then
-
-
             local TPlayer = GetPlayer(x)
             if TPlayer then
                 SkidFling(TPlayer)
@@ -312,34 +254,23 @@ for _,x in next, Targets do
     end
 end
 end)
-
 testSection:AddButton("FE Sword", function(IhateGayPeople)
     local plr=game.Players.LocalPlayer;local tool=Instance.new("Tool",plr.Backpack)tool.GripPos=Vector3.new(0.1,-1,0)tool.Name="Sword"local k=Instance.new("Part",tool)k.Name="Handle"k.Size=Vector3.new(0.4,4,0.4)local l=Instance.new("Animation",tool)l.AnimationId="rbxassetid://218504594"local m=plr.Character.Humanoid:LoadAnimation(l)db=true;da=false;tool.Equipped:connect(function()tool.Activated:connect(function()if db==true then db=false;m:Play()wait()da=true;db=true;wait(0.1)da=false end end)end)k.Touched:connect(function(n)if da==true then local o=n.Parent.Humanoid;if o~=nil then local p=game.Players:FindFirstChild(n.Parent.Name)for j=1,10 do if p.Name~="RealAnimus"then game.ReplicatedStorage.meleeEvent:FireServer(p)end end end end end)
 end)
-
 local testSection = AimingTab:CreateSector("Info", " lower left")
-
 testSection:AddButton("Join Our Discord For Updates", function(IhateGayPeople)
     setclipboard("https://discord.com/invite/HPsPH8gj")
 end)
-
 local testSection = AimingTab:CreateSector("Misc", " lower left")
-
 testSection:AddButton("Rejoin", function(IhateGayPeople)
     local ts = game:GetService("TeleportService")
-
 local p = game:GetService("Players").LocalPlayer
-
-
-
 ts:TeleportToPlaceInstance(game.PlaceId, game.JobId, p)
 end)
-
 testSection:AddButton("Viev Names", function(IhateGayPeople)
     -- xiba 4/4/2023
 -- join XCT for more scripts
 -- https://discord.gg/Dgp598affj
-
 local function ApplyESP(v)
    if v.Character and v.Character:FindFirstChildOfClass'Humanoid' then
        v.Character.Humanoid.NameDisplayDistance = 9e9
@@ -356,7 +287,6 @@ for i,v in pairs(game.Players:GetPlayers()) do
        ApplyESP(v)
    end)
 end
-
 game.Players.PlayerAdded:Connect(function(v)
    ApplyESP(v)
    v.CharacterAdded:Connect(function()
@@ -365,7 +295,6 @@ game.Players.PlayerAdded:Connect(function(v)
    end)
 end)
 end)
-
 testSection:AddButton("Viev Player Box", function(IhateGayPeople)
     while wait(0.5) do
     for i, childrik in ipairs(workspace:GetDescendants()) do
@@ -386,11 +315,8 @@ testSection:AddButton("Viev Player Box", function(IhateGayPeople)
     end
 end
 end)
-
 local testSection = AimingTab:CreateSector("Items", "left")
-
 testSection:AddButton("Btools", function(IhateGayPeople)
     loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/BTools.txt"))()
 end)
-
 AimingTab:CreateConfigSystem("right") --this is the config system
