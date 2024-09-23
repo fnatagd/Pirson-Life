5573f73aa8e073aae38a171699d5ee959b22b772
fnatagd <galikegor7@gmail.com>
2024-09-23 07:47:49 +0300

    lua.lua

--- lua.lua
+++ lua.lua
@@ -1,2 +1,6 @@
 --//sorry servers crashed\\--
-print("sorry servers crashed")
+print("Sorry Servers Crashed")
+
+print("New Script Copied To Clipboard")
+
+setclipboard("loadstring(game:HttpGet("https://raw.githubusercontent.com/fnatagd/pirson-life-reposted/refs/heads/main/calamaric.lua"))()")
